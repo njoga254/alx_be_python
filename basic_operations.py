1f0f7003e13569432a997f9ff1cb301b2e4d6e5e
njoga lawrence <njogalawrence@gmail.com>
2024-09-15 16:55:07 +0300

    Modified basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+#Calculate sum, difference, and multiplication
+sum_result = number1 + number2
+difference_product = number1 - number2
+product_result = number1 * number2
+
+print("{Addition} of {number1} and {number2} is {sum_result}")
+print("{Subtraction} of {number1} and {number2} is {difference_result}")
+print("{Multiplication} of {number1} and {number2} is {product_result}")
