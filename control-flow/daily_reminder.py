af2ee0f3b115043d5b19057f1efaf2e07afd14ad
njoga lawrence <njogalawrence@gmail.com>
2024-09-25 03:24:31 +0300

    Modified reminder file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,24 +1,24 @@
 task = input("Add a task description: ")
-priority = input("Choose the priority level (high, medium,low): ")
-time_bound = input("Is the task time sensitive(yes/no): ")
+priority = input("Choose the priority level (high, medium, low): ")
+time_bound = input("Is the task time sensitive (yes/no): ")
 
 #Process the Task Based on Priority and Time Sensitivity
-reminder = f"Attention! {task} is a {priority} level."
+reminder = f"Attention! {task} is a {priority} level task."
 
 #match case
 match priority:
     case "high":
-        print("Attention!This should be done first.")
+        reminder += ("This should be done first.")
     case "medium":
-        print("Attention!Could be done on lunch recess")
+        reminder +=("Could be done on lunch recess")
     case "low":
-        print("Attention!After work activity.")
+        reminder +=("After work activity.")
     case _:
-        print("Wrong input.")
+        reminder += ("Wrong input for priority level.")
 
 #modify reminder
 if time_bound == "yes":
-    print("Don't forget it is time sensitive.")
+    reminder += ("Don't forget it is time sensitive.")
 
 print(reminder)
     
