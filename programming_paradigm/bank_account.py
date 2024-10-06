15349b37de69acab77cd7f923a20719fecab9300
njoga lawrence <njogalawrence@gmail.com>
2024-10-06 13:39:06 +0300

    programming_paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__(self, initial_balance = 0):
+        self.__account_balance = initial_balance #encapsulation
+    
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited: {amount}")
+        else:
+            print(f"Amount must be a positive.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrawal of {amount} successful.")
+            return True
+        else:
+            print("Failed to withdraw {amount}.")
+            return False
+        
+    def display_balance(self):
+        print(f"Current balance: {self.__account_balance}")
+
