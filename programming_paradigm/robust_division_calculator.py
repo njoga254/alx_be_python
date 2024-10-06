662c685542487071dc3aed7e035a054b4e4c148f
njoga lawrence <njogalawrence@gmail.com>
2024-10-06 18:55:04 +0300

    programming_paradigm

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -18,7 +18,7 @@ def safe_divide(numerator, denominator):
         
         
         result = num / denom
-        return f"Result: {result:.1f}"
+        return f"Result: {result}"
     
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
